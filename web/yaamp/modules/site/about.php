d7aeeabc63da76b66113d71b55fb2e8f81b25291
Original-Tasty <46746362+Original-Tasty@users.noreply.github.com>
2021-03-28 13:05:29 -0400

    Update about.php

--- web/yaamp/modules/site/about.php
+++ web/yaamp/modules/site/about.php
@@ -1,14 +1,10 @@
 
 
-<h2>A few words to present the site and myself</h2>
+<h2>A few words to present the site</h2>
 
 <div style='padding-left: 25px; width: 640px;'>
 
-<p>BitcoinIL official mining Pool.</p>
-
-<p>BitcoinIL</p>
-
-<p>2021 1st of April</p>
+<p>Mining Pool.</p>
 
 </div>
 
