4d10d56701459b5cb066b696836fa96060ebb342
Original-Tasty <quentin.neveu@hotmail.ca>
2021-05-08 23:43:43 -0400

    reset2

---
+++ web/yaamp/modules/site/about.php
@@ -0,0 +1,12 @@
+
+
+<h2>A few words to present the site</h2>
+
+<div style='padding-left: 25px; width: 640px;'>
+
+<p>Mining Pool.</p>
+
+</div>
+
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
