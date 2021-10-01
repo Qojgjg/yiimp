3bd17cd5c15e4583a55365f5f7c8a1a7509d8160
Original-Tasty <quentin.neveu@hotmail.ca>
2021-10-01 17:22:01 -0400

    Update coinbase.cpp

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -695,6 +695,7 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 	//if(coind->txmessage)
 	//	strcat(templ->coinb2, "00");
         if(strcmp(coind->symbol, "FLO") == 0){if(coind->txmessage){strcat(templ->coinb2, "00");}} //fixes FlorinCoin
+        if(strcmp(coind->symbol, "SHTX") == 0){if(coind->txmessage){strcat(templ->coinb2, "00");}}
 	strcat(templ->coinb2, "00000000"); // locktime
 
 	coind->reward = (double)available/100000000*coind->reward_mul;
